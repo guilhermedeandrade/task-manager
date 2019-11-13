e387cb083bf9a35fa691bd27f2c06f39c1169dfa
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2019-11-13 16:43:16 +0000

    feat: normalize avatar images with sharp

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const multer = require('multer')
+const sharp = require('sharp')
+
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 
@@ -103,7 +105,13 @@ router.post(
   auth,
   upload.single('avatar'),
   async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer()
+
+    req.user.avatar = buffer
+
     await req.user.save()
     res.send()
   },
@@ -126,7 +134,7 @@ router.get('/users/:id/avatar', async (req, res) => {
       throw new Error('user avatar not found')
     }
 
-    res.set('Content-Type', 'image/jpg')
+    res.set('Content-Type', 'image/png')
     res.send(user.avatar)
   } catch (err) {
     res.status(404).send({ error: err.message })
